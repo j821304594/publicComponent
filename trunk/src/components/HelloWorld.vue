059a9fc882a8e570674f56d97883bc2eb27ac87d
jijiong <jijiong@ky-tech.com.cn>
2019-08-10 14:50:14 +0800

    添加复制表格到文本域

--- trunk/src/components/HelloWorld.vue
+++ trunk/src/components/HelloWorld.vue
@@ -1,85 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="el-textarea" style="position: relative; width: 306px; height: 206px; overflow: hidden;" contenteditable="true">
+      <textarea name="textarea" id="textarea" style="width: 300px; height: 200px;" border="1"></textarea>
+    </div>
   </div>
 </template>
 
@@ -90,7 +13,66 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  mounted() {
+    document.getElementById('textarea').onpaste = () => {
+      this.pasteTable(event);
+    }
+  },
+  methods: {
+    createNode (str) {
+      let doc = new DOMParser().parseFromString(str, 'text/html');
+      let table = doc.querySelector('#clipTable');
+      return table;
+    },
+    pasteTable(e) {
+      let that = this
+      if ( e.clipboardData ) {
+        console.log(e.clipboardData.getData('Text'))
+        var clipData = e.clipboardData.getData('Text')
+        var data = clipData.split('\n')
+        var dataHtml = ''
+        var allData = []
+        for (var i = 0; i < data.length; i++) {
+          var eachData = data[i].split('\t')
+          allData.push(eachData)
+        }
+        console.log('allData', allData)
+        if (allData.length === 1) {   //如果长度为1，说明只是复制的文本
+          return false
+        } else if (allData.length > 1) {   //如果长度超过1，说明复制的是表格
+          // let tableDom = document.createElement('table')
+          // tableDom.id = 'clipTable'
+          // document.getElementById('editable').appendChild(tableDom)
+          var dataHtml = '<table id="clipTable" border="1" cellpadding="0" cellspacing="0">'
+          allData.forEach((item, index) => {
+            dataHtml += '<tr class="datarow">'
+            item.forEach((el, i) => {
+              dataHtml += '<td>' + (el || '&nbsp;') + '</td>'
+            })
+            dataHtml += '</tr>'
+          })
+          dataHtml += '</table>'
+          console.log(that.createNode(dataHtml))
+          document.getElementsByClassName('el-textarea')[0].appendChild(that.createNode(dataHtml))
+          document.getElementById('clipTable').style = 'position: absolute; left: 2px; top: 2px; max-width: 300px; max-height: 200px; font-size: 12px'
+          // console.log('value', document.getElementById('textarea').value)
+          setTimeout(() => {
+            document.getElementById('textarea').value = ''
+          }, 0);
+          that.$nextTick(() => {
+            const tableH = document.getElementById('clipTable').offsetHeight;
+            console.log('tableH', tableH)
+            if (tableH > 200) {
+
+            }
+          })
+        }
+      } else {
+        console.log('non-chrome');
+      }
+    },
+  },
 }
 </script>
 
